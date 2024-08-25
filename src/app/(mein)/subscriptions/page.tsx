ab24b2d53a5c4a83a84a58d7f7e0ded88d0bcd45
Gocha <gochazautashvili2017@gmail.com>
2024-08-25 14:58:22 +0400

    dynamic import

--- src/app/(mein)/subscriptions/page.tsx
+++ src/app/(mein)/subscriptions/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import SubscriptionVideos from "./SubscriptionVideos";
+import dynamic from "next/dynamic";
+import { Loader2 } from "lucide-react";
+const SubscriptionVideos = dynamic(() => import("./SubscriptionVideos"), {
+  ssr: false,
+  loading: () => <Loader2 className="mx-auto my-10 animate-spin" />,
+});
 
 const SubscriptionPage = () => {
   return <SubscriptionVideos />;
