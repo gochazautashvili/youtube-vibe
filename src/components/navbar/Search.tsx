d7f85b8ed2afc52236388d16cd5b9f6b700d44ff
Gocha <gochazautashvili2017@gmail.com>
2024-08-25 21:17:00 +0400

    1.7

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -33,7 +33,7 @@ const Search = ({ className }: Props) => {
         placeholder="Search"
       />
       <Button type="submit" className="h-full rounded-none dark:bg-black">
-        <SearchIcon />
+        <SearchIcon className="dark:fill-white" />
       </Button>
     </form>
   );
